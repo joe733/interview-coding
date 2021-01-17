3c5e91ae1d8fbd85e0917e8585b10eddfabca190
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2021-01-17 23:19:37 +0530

    feat: adds readme, gitignore

--- prob-01.py
+++ prob-01.py
@@ -1,16 +1,11 @@
-'''
-	A file contains a set of 30 multiple choice questions with four choices among which one is a correct answer. Write a python program to display and evaluate the answers in such a way that the questions appear in a random order and the choices are also jumbled.
-'''
-
 import csv
 from random import sample
-# import pandas
 
 
 class Quiz:
     def __init__(self) -> None:
         self.problem = dict()
-        self.dip_qp, cpd_ans = [], []
+        self.dip_qp, self.cpd_ans = [], []
         self.crct_ans, self.marks = 0, 0
 
     def read_csv(self) -> None:
